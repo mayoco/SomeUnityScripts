22d79b11f73717e6984a6dbf5cc941d70108a935
mayoco <ma_yo233@sina.cn>
2020-05-18 16:45:37 +0800

    编辑器扩展案例3.一键重启项目

---
+++ EditorExtension/3.ReopenProject/Editor/ReopenProject.cs
@@ -0,0 +1,18 @@
+//编辑器扩展案例3.一键重启项目
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace EditorExtension
+{
+    public class ReopenProject
+    {
+        [MenuItem("编辑器扩展/3.ReopenProject &r")]
+        public static void DoReopenProject() 
+        {
+            EditorApplication.OpenProject(Application.dataPath.Replace("Assets",string.Empty));
+        }
+
+    }
+}
