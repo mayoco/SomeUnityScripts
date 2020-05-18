87e974a1544f587f9c8751bce43fc8a3eef8e9e9
mayoco <ma_yo233@sina.cn>
2020-05-18 16:35:44 +0800

    记录绑定物体的信息

---
+++ EditorExtension/2.CreateComponentCode/Editor/BindInfo.cs/BindInfo.cs
@@ -0,0 +1,12 @@
+//记录绑定物体的信息 
+namespace EditorExtension
+{
+    public class BindInfo
+    {
+        public string FindPath;
+
+        public string ComponentName;
+
+        public string Name;
+    }
+}
