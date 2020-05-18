e878b87432946eda65f43197d5df18d4a7b150de
mayoco <ma_yo233@sina.cn>
2020-05-18 16:31:28 +0800

    由代码CreateUIRootWindow.cs生成的物体上所附有的代码

---
+++ EditorExtension/1.SetupUIRoot/UIRoot.cs
@@ -0,0 +1,30 @@
+//由代码CreateUIRootWindow.cs生成的物体上所附有的代码 并且生成的时候自动赋值
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EditorExtension
+{
+    public class UIRoot : MonoBehaviour
+    {
+        public Transform Bg;
+        public Transform Common;
+        public Transform PopUp;
+        public Transform Forward;
+
+        [SerializeField]
+        private Canvas RootCanvas;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
