8d6c9b2d40774d40e78d7b13a63e3f92a3c58f42
mayoco <ma_yo233@sina.cn>
2020-11-24 16:18:38 +0800

    Update ReadWriteToFile.cs
    从文件中读取ip地址进行设置

--- ReadWriteToFile.cs
+++ ReadWriteToFile.cs
@@ -10,6 +10,7 @@ public class ReadWriteToFile : MonoBehaviour
 {//读写txt到hololens文件系统中 具体路径为 User Folders \ LocalAppData \ 相应app文件夹 \ LocalState \
     //通过windows device portal 可以删除并上传新的txt文件,达到类似修改配置文件的效果
     public Text debugText;
+    public TcpClient tcpClient;//tcp客户端脚本
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +20,7 @@ public class ReadWriteToFile : MonoBehaviour
         if (!File.Exists(path))
         {
             // Create a file to write to.
-            string createText = "Hello and Welcome" + Environment.NewLine;
+            string createText = "192.168.137.243" + Environment.NewLine;
             File.WriteAllText(path, createText, Encoding.UTF8);
         }
 
@@ -31,6 +32,7 @@ public class ReadWriteToFile : MonoBehaviour
         // Open the file to read from.
         string readText = File.ReadAllText(path);
         if (debugText) debugText.text = readText;
+        if (tcpClient) tcpClient.ip = readText.Substring(0,15);//从文件中读取ip地址进行设置
     }
 
     // Update is called once per frame
